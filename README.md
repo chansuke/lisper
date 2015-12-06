680cdda557f1dbae136dce077b8d9ecbb6426907
chansuke <moonset20@gmail.com>
2015-12-06 16:47:07 +0900

    Add parser

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 This is a original lisp like language written in C.
 I refered these two pages and using mpc for parser.
 
-www.buildyourownlisp.com
+www.buildyourownlisp.com<br>
 http://mattn.kaoriya.net/software/build_your_own_programming_language.html
 
--- parsing.c
+++ parsing.c
@@ -22,7 +22,22 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
-	puts("Lisper Version 0.1");
+/*Parser*/
+  mpc_parser_t* Number   = mpc_new("Number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lispy    = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                    \
+　　  number   : /-?[0-9]+/ ;                            \
+      operator : '+' | '-' | '*' | '/' ;                 \
+      expr     : <number> | '(' <operator> <expr>+ ')' ; \
+      lispy    : /^/ <operator> <expr>+ /$/ ;            \
+    ",
+    Number, Operator, Expr, Lispy);
+
+	puts("Lisper Version 0.2");
 	puts("Press Ctrl+c to Exit\n");
 
 	while (1) {
@@ -30,10 +45,20 @@ int main(int argc, char** argv) {
     char*input=readline("lisper> ");
     add_history(input);
 
-		printf("No you're a %s", input);
-    free(input);
+    mpc_result_t r;
+    if (mac_parse("<stdin>", input, Lispy, &r)) {
+      /* On success print delete the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+	  }
 
-	}
+    free(input);
+  }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
 	return 0;
 }
