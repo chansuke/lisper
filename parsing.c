4622f103a0735cd97960848f274af3e5944930b7
chansuke <moonset20@gmail.com>
2015-12-06 16:49:07 +0900

    Modify

--- parsing.c
+++ parsing.c
@@ -26,16 +26,16 @@ int main(int argc, char** argv) {
   mpc_parser_t* Number   = mpc_new("Number");
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr     = mpc_new("expr");
-  mpc_parser_t* Lispy    = mpc_new("lispy");
+  mpc_parser_t* Lisper    = mpc_new("lisper");
 
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                    \
 　　  number   : /-?[0-9]+/ ;                            \
       operator : '+' | '-' | '*' | '/' ;                 \
       expr     : <number> | '(' <operator> <expr>+ ')' ; \
-      lispy    : /^/ <operator> <expr>+ /$/ ;            \
+      lisper   : /^/ <operator> <expr>+ /$/ ;            \
     ",
-    Number, Operator, Expr, Lispy);
+    Number, Operator, Expr, Lisper);
 
 	puts("Lisper Version 0.2");
 	puts("Press Ctrl+c to Exit\n");
