001c8982a62c504d575bb4478eeedfd285f07bb7
chansuke <moonset20@gmail.com>
2015-12-13 23:30:50 +0900

    update

--- error_handling.c
+++ error_handling.c
@@ -19,3 +19,27 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #include <editline/history.h>
 #endif
+
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+enum { LVAL_NUM, LVAL_ERR };
+
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
