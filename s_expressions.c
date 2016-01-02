41ac79fc6eba9c1927ee156bc738baa103a5543a
chansuke <moonset20@gmail.com>
2016-01-02 16:57:34 +0900

    Add macro

--- s_expressions.c
+++ s_expressions.c
@@ -154,6 +154,9 @@ void lval_print(lval* v) {
   }
 }
 
+#define LASSERT(args, cond, err) \
+  if (!(cond)) { lval_del(args); return lval_err(err); }
+
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
 lval* builtin_op(lval* a, char* op) {
