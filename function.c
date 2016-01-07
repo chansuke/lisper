70e75b15ca80f5896356d0a16fd62dd257291d8d
chansuke <moonset20@gmail.com>
2016-01-08 01:01:02 +0900

    Add lambda and more

--- function.c
+++ function.c
@@ -1,3 +1,86 @@
 #include "mpc.h"
 
 #ifdef _WIN32
+
+struct lval {
+  int type;
+
+  long num;
+  char* err;
+  char* sym;
+
+  lbuiltin builtin;
+  lenv* env;
+  lval* formals;
+  lval* body;
+
+  int count;
+  lval** cell;
+};
+
+lval* lval_lambda(lval* formals, lval* body) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_FUN;
+
+  /* Set Builtin to Null */
+  v->builtin = NULL;
+
+  /* Build new environment */
+  v->env = lenv_new();
+
+  /* Set Formals and Body */
+  v->formals = formals;
+  v->body = body;
+  return v;
+}
+
+/* for deletetion */
+case LVAL_FUN:
+  if (!v->builtin) {
+    lenv_del(v->env);
+    lenv_del(v->formals);
+    lenv_del(v->body);
+  }
+break;
+
+/* for copying */
+case LVAL_FUN:
+  if (!v->builtin) {
+    x->builtin = v->builtin;
+  } else {
+    x->builtin = NULL;
+    x->env = lenv_copy(v-env);
+    x->formals = lval_copy
+  }
+break;
+
+/* for printing */
+case LVAL_FUN:
+  if (v->builtin) {
+    printf("<builtin>");
+  } else {
+    printf("")
+  }
+break;
+
+/* lambda */
+lval* builtin_lambda(lenv* e, lval* a) {
+  /* Check Two arguments, each of which are Q-Expressions */
+  LASSERT_NUM("\\", a, 2);
+  LASSERT_TYPE("\\", a, 0, LVAL_QEXPR);
+  LASSERT_TYPE("\\", a, 1, LVAL_QEXPR);
+
+  /* Check first Q-Expression contains only Symbols */
+  for (int i = 0; i < a->cell[0]->count; i++) {
+    LASSERT(a, (a->cell[0]->cell[i]->type == LVAL_SYM),
+      "Cannot define non-symbol. Got %s, Expected %s.",
+      ltype_name(a->cell[0]->cell[i]->type),ltype_name(LVAL_SYM));
+  }
+
+  /* Pop first two arguments and pass them to lval_lambda */
+  lval* formals = lval_pop(a, 0);
+  lval* body = lval_pop(a, 0);
+  lval_del(a);
+
+  return lval_lambda(formals, body);
+}
