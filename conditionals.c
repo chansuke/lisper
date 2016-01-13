55f2a1787ac3442959dc6defa52fbd4226cd569a
chansuke <moonset20@gmail.com>
2016-01-13 23:42:02 +0900

    Start implement conditional

---
+++ conditionals.c
@@ -0,0 +1,16 @@
+/* Ordering */
+lval* builtin_gt(lenv* e, lval* a) {
+  return builtin_ord(e, a, ">");
+}
+
+lval* builtin_lt(lenv* e, lval* a) {
+  return builtin_ord(e, a, "<");
+}
+
+lval* builtin_ge(lenv* e, lval* a) {
+  return builtin_ord(e, a, ">=");
+}
+
+lval* builtin_le(lenv* e, lval* a) {
+  return builtin_ord(e, a, "<=");
+}
